2b09f33db3731826b97901bf1eec57f91a920a8a
Michaela Campbell James <3015716c@student.gla.ac.uk>
2026-02-06 14:30:31 +0000

    Chaps 4,5,6

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {}
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    context_dict['categories'] = category_list
+
+    return render(request, 'rango/index.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+    
+    return render(request, 'rango/category.html', context=context_dict)
