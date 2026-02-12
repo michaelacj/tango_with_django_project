c7171242d152fcd68376835b702dcc5e555c5bb1
Michaela Campbell James <3015716c@student.gla.ac.uk>
2026-02-12 22:18:35 +0000

    Chap 8 & 9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'tango_with_django_project.urls'
 
+LOGIN_URL = 'rango:login'  
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -102,6 +104,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+PASSWORD_HASHERS = (
+'django.contrib.auth.hashers.PBKDF2PasswordHasher', 
+'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher', 
+)
 
 
 # Internationalization
